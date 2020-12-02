438b5d6b303b65107124184697982c511d7ae52e
Barry Buitelaar <barry.buitelaar@inqdo.com>
2020-12-02 18:08:43 +0100

    Team3

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
   <head>
 
     <style>
+      body {
+        background-color: coral;
+      }
+
       h1 {
         margin: 50px 0 50px 0;
       }
@@ -19,7 +23,14 @@
         margin-right: auto;
         margin-top: 50px;
         width: 20%;
+        -webkit-animation:spin 4s linear infinite;
+        -moz-animation:spin 4s linear infinite;
+        animation:spin 4s linear infinite;
       }
+
+      @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
+      @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
+      @keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
     </style>
 
   </head>
@@ -27,17 +38,18 @@
   <body>
 
     <?php
-      $database = new PDO('mysql:host=HOSTNAME;dbname=rds01', 'admin', 'PASSWORD');
+      $database = new PDO('mysql:host=rds01-dev-cluster.cluster-czop7ga5cpcj.eu-west-1.rds.amazonaws.com;dbname=rds01', 'admin', 'Abc123!!');
       $statement = $database->query('SELECT * FROM `teams`');
       foreach ($statement->fetchAll() as $row) {
           echo sprintf('<h1>%s</h1>', $row['team_name']);
       }
     ?>
-  
+
+    <h1>De pizza was lekker! Mandarijnen ook trouwens.</h1>
     <p>Als het goed is zie je hier de naam van je team. Dit wordt uit de Aurora Serverless database gehaald.</p>
 
     <img src="docker-logo.jpg" alt="Docker Logo">
-  
+
   </body>
 
 </html>
